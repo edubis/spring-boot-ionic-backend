1fb8fdc05f8b96f5c497b51bd2b4d5370fe28e1e
letrindade <lucas.eduardo.t.silva@gmail.com>
2019-04-29 11:39:55 -0300

    Criado dominio Categoria

--- src/main/java/com/curso/resources/CategoriaResource.java
+++ src/main/java/com/curso/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.dominio.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1  = new Categoria(1, "Informática");
+		Categoria cat2  = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }

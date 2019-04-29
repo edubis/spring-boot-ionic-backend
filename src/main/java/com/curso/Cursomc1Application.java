41f7542affc14fa59b5f84be3654caf0999af5b3
letrindade <lucas.eduardo.t.silva@gmail.com>
2019-04-29 13:38:28 -0300

    criando operacao de instanciacao

--- src/main/java/com/curso/Cursomc1Application.java
+++ src/main/java/com/curso/Cursomc1Application.java
@@ -1,13 +1,33 @@
 package com.curso;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc1Application {
+import com.curso.dominio.Categoria;
+import com.curso.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc1Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		
+	}
+	
+	
 
 }

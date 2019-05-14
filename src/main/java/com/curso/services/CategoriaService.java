8dc869c0c4a573500b05dfb8b17b7034e505dd3c
letrindade <lucas.eduardo.t.silva@gmail.com>
2019-05-14 08:45:18 -0300

    Realizando validacao com BeanValidation

--- src/main/java/com/curso/services/CategoriaService.java
+++ src/main/java/com/curso/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.curso.dominio.Categoria;
+import com.curso.dto.CategoriaDTO;
 import com.curso.repository.CategoriaRepository;
 import com.curso.services.exception.ObjectNotFoundException;
 
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage (Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest  = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }

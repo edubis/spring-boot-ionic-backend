72ace376680f230e06d60d718610503047ff1a48
letrindade <lucas.eduardo.t.silva@gmail.com>
2019-05-14 09:09:55 -0300

    Personalizando retorno da Validacao

---
+++ src/main/java/com/curso/resources/exception/FieldMessage.java
@@ -0,0 +1,37 @@
+package com.curso.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String feldName;
+	private String message;
+
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String feldName, String message) {
+		super();
+		this.feldName = feldName;
+		this.message = message;
+	}
+
+	public String getFeldName() {
+		return feldName;
+	}
+
+	public void setFeldName(String feldName) {
+		this.feldName = feldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
